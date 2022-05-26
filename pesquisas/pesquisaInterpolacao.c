f56ebc97f80227bda7ee1ba9bd6a16ee1fdec883
Arthur Henrique Cavalcanti <50219237+Stuepp@users.noreply.github.com>
2022-05-26 17:32:19 -0300

    Update pesquisaInterpolacao.c

--- pesquisas/pesquisaInterpolacao.c
+++ pesquisas/pesquisaInterpolacao.c
@@ -3,7 +3,7 @@
 int pesquisaInterpolacao(int chave, int v[], int n){
     int ini = 0, meio, fim = n-1;
     while(ini <= fim){
-        meio = ((fim - ini) * (chave - v[ini])) / (v[fim] - v[ini]);
+        meio = ini + ((fim - ini) * (chave - v[ini])) / (v[fim] - v[ini]);
         if(chave < v[meio]){
             fim = meio - 1;
         } else if(chave > v[meio]){
